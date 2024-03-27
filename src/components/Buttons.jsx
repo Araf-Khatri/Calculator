1b840662be5650fc789e15831fef6a3b67fb2568
Araf Khatri <arafkhatri0@gmail.com>
2024-03-27 23:46:59 +0530

    style change and added icons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,24 +1,25 @@
-import { IoBackspaceOutline } from "react-icons/io5";
-import { FaDivide } from "react-icons/fa6";
+import { IoBackspace } from "react-icons/io5";
+import { FaDivide, FaMinus, FaPlus, FaEquals } from "react-icons/fa6";
+
 const button = [
   { label: "C", value: "clear" },
-  { label: <IoBackspaceOutline />, value: "backspace" },
+  { label: <IoBackspace />, value: "backspace" },
   { label: "1", value: "1" },
   { label: "2", value: "2" },
   { label: "3", value: "3" },
-  { label: "+", value: "+", classname: "addition" },
+  { label: <FaPlus />, value: "+", classname: "addition glow op" },
   { label: "4", value: "4" },
   { label: "5", value: "5" },
   { label: "6", value: "6" },
   { label: "7", value: "7" },
   { label: "8", value: "8" },
   { label: "9", value: "9" },
-  { label: "-", value: "-" },
+  { label: <FaMinus />, value: "-", classname: "glow op" },
   { label: ".", value: "." },
-  { label: "0", value: "0", classname:"zero" },
-  { label: <FaDivide height={10} width={10} />, value: "/" },
-  { label: "x", value: "*" },
-  { label: "=", value: "=", classname: "equal" },
+  { label: "0", value: "0", classname: "zero" },
+  { label: <FaDivide />, value: "/", classname: "glow op" },
+  { label: <FaPlus />, value: "*", classname: "glow multiply op" },
+  { label: <FaEquals />, value: "=", classname: "equal op" },
 ];
 
 const Buttons = ({ onButtonClick }) => (
